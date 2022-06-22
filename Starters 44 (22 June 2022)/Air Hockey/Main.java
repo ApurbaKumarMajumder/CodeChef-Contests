3b7655765f2acadd9ca48dffd6f82026c6545efd
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-06-22 21:07:58 +0530

    Air Hockey Question

---
+++ Starters 44 (22 June 2022)/Air Hockey/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t > 0){
+            int Alice, Bob;
+            Alice = scn.nextInt();
+            Bob = scn.nextInt();
+
+            if(Alice >= Bob){
+                System.out.println(7-Alice);
+            } else {
+                System.out.println(7 - Bob);
+            }
+            t--;
+        }
+    }
+}
