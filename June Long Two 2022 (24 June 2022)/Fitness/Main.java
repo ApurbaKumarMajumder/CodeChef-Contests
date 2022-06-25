988150b3747274de9642a204518068d2816d9da9
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-06-25 10:34:03 +0530

    fitness question done

---
+++ June Long Two 2022 (24 June 2022)/Fitness/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    static long mod = 1000000007;
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0){
+            int n = scn.nextInt();
+            System.out.println(n * 10);
+        }
+    }
+
+}
